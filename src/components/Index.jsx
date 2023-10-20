93abf09212891a2d97e38ad4b0c972dff0c71827
Kevinelorigi <Kevinelorigi2002@gmail.com>
2023-10-20 14:50:24 -0500

    Seven

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -8,22 +8,17 @@ import Tres from "../img/Tres.png";
 import Cuatro from "../img/Cuatro.png";
 import Hombrr from "../img/Hombrr.jpg";
 
-
 const Index = () => {
   return (
     <div>
-      <div className="flex relative bg-[#D8D9D8]">
-        {/* Mobile View */}
+      <div name="principal" className="flex relative bg-[#D8D9D8]">
         <div className="w-full md:w-1/2 h-screen">
           <img
             src={Mujer}
             alt="Imagen Izquierda"
             className="w-full h-full object-cover"
           />
-          <div
-            className="absolute bottom-0 w-full md:w-[50%] h-1/3 bg-[#3E34BA]"
-            style={{ zIndex: 1 }}
-          >
+          <div className="absolute bottom-0 w-full md:w-[50%] h-1/3 bg-[#3E34BA]">
             <div className="text-white text-4xl md:text-6xl ml-2 md:ml-7">
               <p className="font-bold">NEW</p>
               <p className="font-bold">COLLECTION</p>
@@ -38,8 +33,6 @@ const Index = () => {
             </div>
           </div>
         </div>
-
-        {/* Tablet and Desktop View */}
         <div className="hidden md:flex w-1/2 h-screen relative">
           <div className="absolute w-full h-full flex items-center justify-center">
             <img src={Hombre} alt="Imagen Derecha" className="w-1/2" />
@@ -64,7 +57,7 @@ const Index = () => {
           </div>
         </div>
       </div>
-      <div className="w-full h-screen bg-white">
+      <div name="secundario" className="w-full h-screen bg-white">
         <div className="text-center mt-20 font-bold text-6xl">
           <h1>OUR SELECTIONS</h1>
         </div>
@@ -72,109 +65,66 @@ const Index = () => {
           <button className="bg-[#3E34BA] px-4 py-2 rounded-lg text-white">
             NUEVOS PRODUCTOS
           </button>
-          <button className="bg-white border border-black px-4 py-2 rounded-lg ">
+          <button className="bg-white border border-black px-4 py-2 rounded-lg">
             PRODUCTOS MAS VENDIDOS
           </button>
         </div>
-
-        <section className="mt-16 w-full place-items-center grid gap-2 p-10 grid-cols-2 md:grid-cols-4">
-          <div className="h-[610px] w-[80%] bg-[#D8D9D8] rounded-lg p-4 flex flex-col items-center justify-center">
-            <div className="bg-white p-0.9 rounded-md">
-              <img
-                src={Una}
-                alt="Primera"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="mt-1 text-center">
-              <div className="text-left pr-[100px]">
-                <p className="text-black font-bold">Lacoste</p>
-                <p className="text-black font-bold">
-                  Camisa Azul Indigo Lacoste
-                </p>
+        <section className="mt-16 grid gap-2 p-10 grid-cols-1 md:grid-cols-4">
+          {Array(4)
+            .fill()
+            .map((_, index) => (
+              <div
+                key={index}
+                className="h-[610px] md:h-auto w-full md:w-[80%] bg-[#D8D9D8] rounded-lg p-4 flex flex-col items-center justify-center md:mx-4 md:mt-8"
+              >
+                <div className="bg-white p-0.9 rounded-md">
+                  <img
+                    src={
+                      index === 0
+                        ? Una
+                        : index === 1
+                        ? Tres
+                        : index === 2
+                        ? Cuatro
+                        : Dos
+                    }
+                    alt={`Imagen ${index}`}
+                    className="w-full h-[480px] object-cover"
+                  />
+                </div>
+                <div className="mt-1 text-center">
+                  <div className="text-left pr-[100px]">
+                    <p className="text-black font-bold">Lacoste</p>
+                    <p className="text-black font-bold">
+                      Camisa Azul Indigo Lacoste
+                    </p>
+                  </div>
+                  <p className="text-[#61615F] font-bold">$100.000</p>
+                </div>
               </div>
-              <p className="text-[#61615F] font-bold">$100.000</p>
-            </div>
-          </div>
-          <div className="h-[610px] w-[80%] bg-[#D8D9D8] rounded-lg p-4 flex flex-col items-center justify-center">
-            <div className="bg-white p-0.9 rounded-md">
-              <img
-                src={Tres}
-                alt="Segunda"
-                className="w-full h-[480px] object-cover"
-              />
-            </div>
-            <div className="mt-1 text-center">
-              <div className="text-left pr-[100px]">
-                <p className="text-black font-bold">Lacoste</p>
-                <p className="text-black font-bold">
-                  Camisa Azul Indigo Lacoste
-                </p>
-              </div>
-              <p className="text-[#61615F] font-bold">$100.000</p>
-            </div>
-          </div>
-          <div className="h-[610px] w-[80%] bg-[#D8D9D8] rounded-lg p-4 flex flex-col items-center justify-center">
-            <div className="bg-white p-0.9 rounded-md">
-              <img
-                src={Cuatro}
-                alt="Tercera"
-                className="w-full object-cover h-[480px]"
-              />
-            </div>
-            <div className="mt-1 text-center">
-              <div className="text-left pr-[100px]">
-                <p className="text-black font-bold">Lacoste</p>
-                <p className="text-black font-bold">
-                  Camisa Azul Indigo Lacoste
-                </p>
-              </div>
-              <p className="text-[#61615F] font-bold">$100.000</p>
-            </div>
-          </div>
-          <div className="h-[610px] w-[80%] bg-[#D8D9D8] rounded-lg p-4 flex flex-col items-center justify-center">
-            <div className="bg-white p-0.9 rounded-md">
-              <img
-                src={Dos}
-                alt="Cuarta"
-                className="w-full h-[480px] object-cover"
-              />
-            </div>
-            <div className="mt-1 text-center">
-              <div className="text-left pr-[100px]">
-                <p className="text-black font-bold">Lacoste</p>
-                <p className="text-black font-bold">
-                  Camisa Azul Indigo Lacoste
-                </p>
-              </div>
-              <p className="text-[#61615F] font-bold">$100.000</p>
-            </div>
-          </div>
+            ))}
         </section>
         <div className="flex justify-center">
-          <button className="bg-white border border-black px-6 py-1 rounded-xl ">
+          <button className="bg-white border border-black px-6 py-1 rounded-xl">
             VER MAS
           </button>
         </div>
-      </div>
-      <div className="bg-[#AC60EB] h-screen flex">
-        <div className="w-1/2">
-          <p className="font-bold text-8xl text-white ml-6">MODERN</p>
-          <p className="font-bold text-8xl mt-10 text-white ml-6">THREADS</p>
-
-          <p className="font-bold text-2xl mt-12 text-white ml-5">Lorem</p>
-
-          <button className="bg-white border border-black px-5 py-1 ml-10 rounded-xl ">
-            SHOP STORE
-          </button>
-        </div>
-        <div className="w-1/2 h-full object-cover flex-row-reverse">
-          <img
-            src={Hombrr}
-            alt="Hombre"
-            className="w-full h-full object-cover"
-          />
-        </div>
+        <footer
+          name="Tercero"
+          className="bg-[#AC60EB] h-screen flex flex-col md:flex-row mb-4 mt-10 md:mb-0"
+        >
+          <div className="w-full md:w-1/2">
+            <p className="font-bold text-8xl text-white ml-6">MODERN</p>
+            <p className="font-bold text-8xl mt-10 text-white ml-6">THREADS</p>
+            <p className="font-bold text-2xl mt-12 text-white ml-5">Lorem</p>
+            <button className="bg-white border border-black px-5 py-1 ml-10 mt-4 rounded-xl">
+              SHOP STORE
+            </button>
+          </div>
+          <div className="w-full md:w-1/2 h-1/2 object-cover">
+            <img src={Hombrr} alt="Hombre" className="w-full h-[973px] mb-4" />
+          </div>
+        </footer>
       </div>
     </div>
   );
