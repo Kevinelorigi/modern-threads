d0b41245d8f806b3ee68c29316b61df18cede33b
Kevinelorigi <Kevinelorigi2002@gmail.com>
2023-10-24 13:41:58 -0500

    Index-First

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -7,6 +7,7 @@ import Dos from "../img/Dos.png";
 import Tres from "../img/Tres.png";
 import Cuatro from "../img/Cuatro.png";
 import Hombrr from "../img/Hombrr.jpg";
+import Mujersota from "../img/Mujersota.png";
 
 const Index = () => {
   return (
@@ -109,7 +110,7 @@ const Index = () => {
             VER MAS
           </button>
         </div>
-        <footer
+        <div
           name="Tercero"
           className="bg-[#AC60EB] h-screen flex flex-col md:flex-row mb-4 mt-10 md:mb-0"
         >
@@ -124,7 +125,55 @@ const Index = () => {
           <div className="w-full md:w-1/2 h-1/2 object-cover">
             <img src={Hombrr} alt="Hombre" className="w-full h-[973px] mb-4" />
           </div>
-        </footer>
+        </div>
+        <div>
+          <div className="h-screen flex">
+            <div className="w-1/2 h-full flex flex-row-reverse bg-[#7CFE80]">
+              <img
+                src={Mujersota}
+                alt="Mujer"
+                className="h-full object-cover mr-auto"
+              />
+            </div>
+            <div className="w-1/2">
+              <div className="w-full h-1/2 flex flex-row justify-around mt-10">
+                <div>
+                  <h1 className="font-bold text-2xl">To Discover</h1>
+                  <p className="text-[#7A7B7A] font-bold">
+                    <ul>WIND BREAKER</ul>
+                    <ul>JACKETS</ul>
+                    <ul>TRENCH & PACKAS</ul>
+                    <ul>SHEATERS</ul>
+                    <ul>POLO SHIRTS & TSHIRTS</ul>
+                    <ul>SHORTS & PANTS</ul>
+                    <ul>SWIM WAER</ul>
+                  </p>
+                </div>
+                <div>
+                  <h1 className="font-bold text-2xl">Our Commiments</h1>
+                  <p className="text-[#7A7B7A] font-bold">
+                    <ul>CONTACT US</ul>
+                    <ul>SHIPPING & RETURNS</ul>
+                    <ul>C.V.G</ul>
+                    <ul>FAQ</ul>
+                  </p>
+                </div>
+              </div>
+              <div className="w-1/2">
+                <div className="ml-36">
+                  <h1 className="font-bold text-2xl">Modern Threads</h1>
+                  <p className="text-[#7A7B7A] font-bold">
+                    <ul>SIZE MATCHING</ul>
+                    <ul>OUR STORES</ul>
+                    <ul>CAREERS</ul>
+                    <ul>OURS STORY</ul>
+                    <ul>LEGAL</ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
